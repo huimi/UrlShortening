dea1515c2228efc42bc745129feb2187b6b60184
Huilan Nancy Hsieh <nancyz1204@gmail.com>
2024-06-04 14:40:14 -0700

    refactor css styles.

--- src/App.js
+++ src/App.js
@@ -8,17 +8,34 @@ function App() {
       <div className="main container">
         <Navbar />
         <div className="introduction">
-          <div className="title">
+          <div className="title-container">
             <h1>More than just shorter links</h1>
             <p>Build your brand’s recognition and get detailed insights 
             on how your links are performing.</p>
-            <Button text="Get Started"/>
+            <Button page="signup" text="Get Started"/>
           </div>
           <img id="mainSvg" alt="iullstration-workding" src={iw} />
         </div>
       </div>
-      <div className="container">
-        <input className="url-form"></input>
+      <div className="container feature">
+        <div className="form-container">
+          <input className="form" placeholder="Shorten a link here..." />
+          <Button page="shorten" text="Shorten It!"/>
+        </div>
+        <div className="record-container">
+          <div className="record">
+            <div>https://frontendmentorio.com</div>
+            <div>
+              <div>https://rel.ink/aBcdE1</div>
+              <Button page="copy" text="Copy"/>
+            </div>
+          </div>
+        </div>
+        <div className="title-container text-center">
+          <h1>Advanced Statistics</h1>
+          <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+        </div>
+        {/* <input className="url-form"></input> */}
         {/* <Button text={"Shorten It!"} /> */}
       </div>
       <div className="container">
